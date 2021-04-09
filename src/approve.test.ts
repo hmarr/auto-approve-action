7d3852f22eb4dbc83b467154e88f068440626fd2
Rory Abraham <rory@expensify.com>
2021-04-09 09:45:40 -0700

    Remove unnecessary 0s from tests

--- src/approve.test.ts
+++ src/approve.test.ts
@@ -16,7 +16,7 @@ test("when a review is successfully created", async () => {
     .post("/repos/hmarr/test/pulls/101/reviews")
     .reply(200, { id: 1 });
 
-  await approve("gh-tok", ghContext(), 0);
+  await approve("gh-tok", ghContext());
 
   expect(core.info).toHaveBeenCalledWith(
     expect.stringContaining("Approved pull request #101")
@@ -36,7 +36,7 @@ test("when a review is successfully created using pull-request-number", async ()
 });
 
 test("without a pull request", async () => {
-  await approve("gh-tok", new Context(), 0);
+  await approve("gh-tok", new Context());
 
   expect(core.setFailed).toHaveBeenCalledWith(
     expect.stringContaining("Make sure you're triggering this")
@@ -48,7 +48,7 @@ test("when the token is invalid", async () => {
     .post("/repos/hmarr/test/pulls/101/reviews")
     .reply(401, { message: "Bad credentials" });
 
-  await approve("gh-tok", ghContext(), 0);
+  await approve("gh-tok", ghContext());
 
   expect(core.setFailed).toHaveBeenCalledWith(
     expect.stringContaining("`github-token` input parameter")
@@ -60,7 +60,7 @@ test("when the token doesn't have write permissions", async () => {
     .post("/repos/hmarr/test/pulls/101/reviews")
     .reply(403, { message: "Resource not accessible by integration" });
 
-  await approve("gh-tok", ghContext(), 0);
+  await approve("gh-tok", ghContext());
 
   expect(core.setFailed).toHaveBeenCalledWith(
     expect.stringContaining("pull_request_target")
@@ -72,7 +72,7 @@ test("when a user tries to approve their own pull request", async () => {
     .post("/repos/hmarr/test/pulls/101/reviews")
     .reply(422, { message: "Unprocessable Entity" });
 
-  await approve("gh-tok", ghContext(), 0);
+  await approve("gh-tok", ghContext());
 
   expect(core.setFailed).toHaveBeenCalledWith(
     expect.stringContaining("same user account")
@@ -84,7 +84,7 @@ test("when the token doesn't have access to the repository", async () => {
     .post("/repos/hmarr/test/pulls/101/reviews")
     .reply(404, { message: "Not Found" });
 
-  await approve("gh-tok", ghContext(), 0);
+  await approve("gh-tok", ghContext());
 
   expect(core.setFailed).toHaveBeenCalledWith(
     expect.stringContaining("doesn't have access")
