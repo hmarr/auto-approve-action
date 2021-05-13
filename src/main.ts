d99be49af80c6d2040e95c55c6aae7c37953fb3f
Rory Abraham <rory@expensify.com>
2021-05-13 14:07:52 -0700

    Fix conditional in main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { approve } from "./approve";
 async function run() {
   const token = core.getInput("github-token", { required: true });
   const prNumber: number = parseInt(core.getInput("pull-request-number"), 10);
-  if (Number.isNaN(prNumber)) {
+  if (!Number.isNaN(prNumber)) {
     await approve(token, github.context, prNumber);
   } else {
     await approve(token, github.context);
