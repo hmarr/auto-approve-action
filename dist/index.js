56094622a0e3158a48285ba223c64075b2ec7bf4
Harry Marr <harry.marr@gmail.com>
2023-03-15 10:44:34 -0500

    Only consider the latest review for a user

--- dist/index.js
+++ dist/index.js
@@ -10093,7 +10093,14 @@ function approve(token, context, prNumber, reviewMessage) {
             core.info(`Current user is ${login}`);
             const prHead = pr.head.sha;
             core.info(`Commit SHA is ${prHead}`);
-            const alreadyReviewed = reviews.some(({ user, state }) => (user === null || user === void 0 ? void 0 : user.login) === login && state === "APPROVED");
+            // Only the most recent review for a user counts towards the review state
+            const latestReviewForUser = [...reviews]
+                .reverse()
+                .find(({ user }) => (user === null || user === void 0 ? void 0 : user.login) === login);
+            const alreadyReviewed = (latestReviewForUser === null || latestReviewForUser === void 0 ? void 0 : latestReviewForUser.state) === "APPROVED";
+            // If there's an approved review from a user, but there's an outstanding review request,
+            // we need to create a new review. Review requests mean that existing "APPROVED" reviews
+            // don't count towards the mergeability of the PR.
             const outstandingReviewRequest = (_b = pr.requested_reviewers) === null || _b === void 0 ? void 0 : _b.some((reviewer) => reviewer.login == login);
             if (alreadyReviewed && !outstandingReviewRequest) {
                 core.info(`Current user already approved pull request #${prNumber}, nothing to do`);
